0ffd844b6af9c0a789c50a158567790e415da12d
NotAriRay <arikarakushi@yahoo.com>
2024-07-21 12:21:15 +0200

    bento grid

--- mansonry.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var elem = document.querySelector('.grid');
-  var msnry = new Masonry(elem, {
-    itemSelector: '.border-container',
-    columnWidth: '.border-container',
-    percentPosition: true
-  });
-});
